c0e12da8ee88c2f2d13305bb4d3d1a86a466415a
Eduardo Soares <eduardosoares.pt@gmail.com>
2016-06-29 16:58:50 +0100

    Fix some misconceptions about echo causing a wrong config

--- src/start.sh
+++ src/start.sh
@@ -1,22 +1,22 @@
 echo $iwface
 # Generate config
 echo "" > config.conf
-echo "FIBMetric \"flat\"\n" >> config.conf
-echo "ClearScreen yes\n" >> config.conf
-echo "AllowNoInt yes\n" >> config.conf
-echo "IpcConnect{\n" >> config.conf
-echo "	MaxConnections 0\n" >> config.conf
-echo "	Host 127.0.0.1\n}" >> config.conf
-echo "UseHysteresis no\n" + "NicChgsPollInt 3.0\n" + "TcRedundancy 2\n" + "MprCoverage 3\n" >> config.conf
+echo "FIBMetric \"flat\"" >> config.conf
+echo "ClearScreen yes" >> config.conf
+echo "AllowNoInt yes" >> config.conf
+echo "IpcConnect{" >> config.conf
+echo "	 MaxConnections 0" >> config.conf
+echo "	 Host 127.0.0.1\n}" >> config.conf
+echo "UseHysteresis no\nNicChgsPollInt 3.0\nTcRedundancy 2\nMprCoverage 3" >> config.conf
 	
 # Interface
-echo "Interface \"">> config.conf
-echo $iwface >> config.conf
-echo "\"\n" >> config.conf
-echo "{\n	 Ip4Broadcast 255.255.255.255\n" >> config.conf
-echo "	 Mode \"mesh\"\n}\n" >> config.conf
+printf "Interface \"">> config.conf
+printf $iwface >> config.conf
+echo "\"" >> config.conf
+echo "{\n	 Ip4Broadcast 255.255.255.255" >> config.conf
+echo "	 Mode \"mesh\"\n}" >> config.conf
 
 echo "\nhello world\n"
 
 # start olsr
-olsrd -f config.conf -d 0
+./olsrd -f config.conf -d 0
